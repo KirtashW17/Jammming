cd0c8ca772d24ce064e7e686acea29104326dd71
Thomas <thomasbalafia@gmail.com>
2018-09-19 20:55:55 +0200

    task 79

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
         name: 'De Trankis',
         artist: 'LDS',
         album: 'Viridarquía',
-        id:'001'
+        id:'002'
       },{
         name: 'De Trankis',
         artist: 'LDS',
@@ -57,18 +57,23 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
   addTrack(track) {
     if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
       return;
-    }
-    let newPlaylist = this.state.playlistTracks.push(track);
+    } else {
+    let newPlaylist = this.state.playlistTracks.slice();
+    newPlaylist.push(track);
     this.setState({playlistTracks : newPlaylist});
+    }
   }
   removeTrack(track){
     let trackToFind = this.state.playlistTracks.find(savedTrack => savedTrack.id==track.id);
     let trackIndex = this.state.playlistTracks.findIndex(savedTrack => savedTrack==track);
-    let newPlaylist = this.state.playlistTracks.splice(trackIndex,1);
+    let newPlaylist = this.state.playlistTracks.slice();
+    newPlaylist.splice(trackIndex,1);
     this.setState({playlistTracks : newPlaylist});
   }
   updatePlaylistName(name){
@@ -77,15 +82,18 @@ class App extends React.Component {
   savePlaylist(){
     const trackUris = this.state.playlistTracks.map(track => track.uri);
       }
+  search(term){
+        console.log(term);
+      }
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
---
+++ src/util/Spotify.js
@@ -0,0 +1,19 @@
+const apiKey = '';
+const Spotify = {
+  getAccessToken()
+
+    {
+
+      if(accessToken)
+
+        return accessToken;
+
+      else if(window.location.href.match())
+
+
+
+
+
+
+
+export default Spotify;
