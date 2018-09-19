ddb950b8cfcc8ebbf5663faebfc9c8c36f0b387b
Thomas <thomasbalafia@gmail.com>
2018-09-19 21:06:59 +0200

    task 79

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -2,16 +2,11 @@ const apiKey = '';
 const Spotify = {
   getAccessToken(){
 
-      if(accessToken) {
-        return accessToken;
-      }
-      else if(window.location.href.match())
-  }
+    if(accessToken) {
+      return accessToken;
+    }
+    else if(window.location.href.match(/access_token=([^&])/) && window.location.href.match(/expires_in=([^&])/)){
 
-
-
-
-
-
+    }
 
 export default Spotify;
